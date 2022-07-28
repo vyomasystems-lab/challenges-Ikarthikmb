1980d93b574d95b8eca7fec2fa16d6ba2f6db132
KarthikMB <36357510+Ikarthikmb@users.noreply.github.com>
2022-07-29 04:16:56 +0530

    verification run test 2 pass

--- level2_design/test_mkbitmanip.py
+++ level2_design/test_mkbitmanip.py
@@ -16,12 +16,12 @@ from model_mkbitmanip import *
 def clock_gen(signal):
     while True:
         signal.value <= 0
-        yield Timer(1) 
+        yield Timer(1)
         signal.value <= 1
-        yield Timer(1) 
+        yield Timer(1)
 
 #################################################################################
-# Sample Default Test 
+# Sample Default Test
 #################################################################################
 @cocotb.test()
 def run_test(dut):
@@ -31,24 +31,26 @@ def run_test(dut):
 
     # reset
     dut.RST_N.value <= 0
-    yield Timer(10) 
+    yield Timer(10)
     dut.RST_N.value <= 1
 
     ######### CTB : Modify the test to expose the bug #############
-    ### Test values
-    func3 = ["001" ,"010" ,"011" ,"100" ,"101" ,"110" ,"111"]
+    # Test values
+    func3 = ["001", "010", "011", "100", "101", "110", "111"]
     # func3 = [hex(int("0b"+c,2))[2:] for c in func3]
-    func7 = ["0000100" ,"0000101" ,"0010000" ,"0010100" ,"0100000" ,"0100100" ,"0110000" ,"0110100"]
+    func7 = ["0000100", "0000101", "0010000", "0010100",
+             "0100000", "0100100", "0110000", "0110100"]
     # func7 = [hex(int("0b"+c,2))[2:] for c in func7]
-    func7_2bit = ["10" ,"11"]
+    func7_2bit = ["10", "11"]
     # func7_2bit = [hex(int("0b"+c,2))[2:] for c in func7_2bit]
     func7_fsri_1bit = 0
-    func7_imm = ["00100" ,"00101" ,"01001" ,"01100" ,"01101"]
+    func7_imm = ["00100", "00101", "01001", "01100", "01101"]
     # func7_imm = [hex(int("0b"+c,2))[2:] for c in func7_imm]
     func7_imm_SHFL = "000010"
-    imm_value_1 = ["00000" ,"00001" ,"00010" ,"00100" ,"00101" ,"10000" ,"10001" ,"10010" ,"11000" ,"11001" ,"11010"]
+    imm_value_1 = ["00000", "00001", "00010", "00100", "00101",
+                   "10000", "10001", "10010", "11000", "11001", "11010"]
     # imm_value_1 = [hex(int("0b"+c,2))[2:] for c in imm_value_1]
-    opcode = ["0010011" ,"0110011"]
+    opcode = ["0010011", "0110011"]
     # opcode = [hex(int("0b"+c,2))[2:] for c in opcode]
 
     # input transaction
@@ -64,7 +66,8 @@ def run_test(dut):
     cocotb.log.info(f'mav_putvalue_instr={hex(int(mav_putvalue_instr))}')
 
     # expected output from the model
-    expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
+    expected_mav_putvalue = bitmanip(
+        mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
 
     # driving the input transaction
     dut.mav_putvalue_src1.value = mav_putvalue_src1
@@ -73,7 +76,7 @@ def run_test(dut):
     dut.EN_mav_putvalue.value = 1
     dut.mav_putvalue_instr.value = mav_putvalue_instr
 
-    yield Timer(1) 
+    yield Timer(1)
 
     # obtaining the output
     dut_output = dut.mav_putvalue.value
@@ -83,11 +86,13 @@ def run_test(dut):
 
     # comparison
     if (dut_output == expected_mav_putvalue):
-	    cocotb.log.info(f'DUT OUTPUT={hex(dut_output)} == EXPECTED{hex(expected_mav_putvalue)} | MATCHED ')
+        cocotb.log.info(
+            f'DUT OUTPUT={hex(dut_output)} == EXPECTED{hex(expected_mav_putvalue)} | MATCHED ')
     else:
-	    cocotb.log.info('--------------------------------------------------')
-	    cocotb.log.info(f'DUT OUTPUT={hex(dut_output)} != EXPECTED={hex(expected_mav_putvalue)} | NOT MATCHED ')
-	    cocotb.log.info('--------------------------------------------------')
+        cocotb.log.info('--------------------------------------------------')
+        cocotb.log.info(
+            f'DUT OUTPUT={hex(dut_output)} != EXPECTED={hex(expected_mav_putvalue)} | NOT MATCHED ')
+        cocotb.log.info('--------------------------------------------------')
     # error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
     # assert dut_output == expected_mav_putvalue, error_message
 
@@ -102,7 +107,7 @@ def run_test2(dut):
 
     # reset
     dut.RST_N.value <= 0
-    yield Timer(10) 
+    yield Timer(10)
     dut.RST_N.value <= 1
 
     ######### CTB : Modify the test to expose the bug #############
@@ -113,61 +118,68 @@ def run_test2(dut):
     # mav_putvalue_instr = 0x101010B3
     # mav_putvalue_instr = 0x40007033
     fp = open("inst_hex.txt", "r")
+    cr = 0
     for line in fp:
-	    mav_putvalue_instr = int(line,16)
-	    # cocotb.log.info(type(mav_putvalue_instr))
-	    cocotb.log.info('--------------------------------------------------')
-	    cocotb.log.info(f'mav_putvalue_instr={hex(int(mav_putvalue_instr))}')
-	    cocotb.log.info(f'mav_putvalue_instr={bin(int(mav_putvalue_instr))}')
-	    mpiv = bin(int(mav_putvalue_instr))
-	    cocotb.log.info(f'{mpiv[2:6]} {mpiv[6:10]} {mpiv[10:14]} {mpiv[14:18]} {mpiv[18:22]} {mpiv[22:26]} {mpiv[26:30]} {mpiv[30:]}')
+        mav_putvalue_instr = int(line, 16)
+        # cocotb.log.info(type(mav_putvalue_instr))
+        cocotb.log.info('--------------------------------------------------')
+        cocotb.log.info(f'--------------Test NO {cr}-------------------------')
+        cocotb.log.info(f'mav_putvalue_instr={hex(int(mav_putvalue_instr))}')
+        cocotb.log.info(f'mav_putvalue_instr={bin(int(mav_putvalue_instr))}')
+        mpiv = bin(int(mav_putvalue_instr))
+        cocotb.log.info(
+            f'{mpiv[2:6]} {mpiv[6:10]} {mpiv[10:14]} {mpiv[14:18]} {mpiv[18:22]} {mpiv[22:26]} {mpiv[26:30]} {mpiv[30:]}')
 
-	    instr=hex(mav_putvalue_instr)[2:]
-	    le=int(instr,16) #convert Hex  to int
-	    le=bin(le)[2:] #convert int to binary
-	    le=le.zfill(32)
-	    length=len(le)
-	    opcode = le[-7::]
-	    func3 = le[length-15:length-12]
-	    func7 = le[length-32:length-25]
-	    func7_imm = le[length-32:length-27]
-	    func7_2bit = le[length-27:length-25]
-	    func7_1bit = le[length-28:length-27]
-	    func7_fsri_1bit = le[length-27:length-26]
-	    func7_imm_SHFL = le[length-32:length-26]
-	    imm_value = le[length-25:length-20]
-	    imm_value_1 = le[length-25:length-20]
-	    fsr_imm_value = le[length-26:length-20]
-	    fsr_imm_value=(int(str(fsr_imm_value),2))
-	    imm_value=(int(str(imm_value),2))
-	    shamt_imm= imm_value & (31)
-	    shamt1= mav_putvalue_src2 & (31)
+        # Debug purpose
+        instr = hex(mav_putvalue_instr)[2:]
+        le = int(instr, 16)  # convert Hex  to int
+        le = bin(le)[2:]  # convert int to binary
+        le = le.zfill(32)
+        length = len(le)
+        opcode = le[-7::]
+        func3 = le[length-15:length-12]
+        func7 = le[length-32:length-25]
+        func7_imm = le[length-32:length-27]
+        func7_2bit = le[length-27:length-25]
+        func7_1bit = le[length-28:length-27]
+        func7_fsri_1bit = le[length-27:length-26]
+        func7_imm_SHFL = le[length-32:length-26]
+        imm_value = le[length-25:length-20]
+        imm_value_1 = le[length-25:length-20]
+        fsr_imm_value = le[length-26:length-20]
+        fsr_imm_value = (int(str(fsr_imm_value), 2))
+        imm_value = (int(str(imm_value), 2))
+        shamt_imm = imm_value & (31)
+        shamt1 = mav_putvalue_src2 & (31)
 
-	    # expected output from the model
-	    expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
+        # expected output from the model
+        expected_mav_putvalue = bitmanip(
+            mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
 
-	    # driving the input transaction
-	    dut.mav_putvalue_src1.value = mav_putvalue_src1
-	    dut.mav_putvalue_src2.value = mav_putvalue_src2
-	    dut.mav_putvalue_src3.value = mav_putvalue_src3
-	    dut.EN_mav_putvalue.value = 1
-	    dut.mav_putvalue_instr.value = mav_putvalue_instr
+        # driving the input transaction
+        dut.mav_putvalue_src1.value = mav_putvalue_src1
+        dut.mav_putvalue_src2.value = mav_putvalue_src2
+        dut.mav_putvalue_src3.value = mav_putvalue_src3
+        dut.EN_mav_putvalue.value = 1
+        dut.mav_putvalue_instr.value = mav_putvalue_instr
+        print(mav_putvalue_instr)
 
-	    yield Timer(1) 
+        yield Timer(1)
 
-	    # obtaining the output
-	    dut_output = dut.mav_putvalue.value
+        # obtaining the output
+        dut_output = dut.mav_putvalue.value
 
-	    # cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
-	    # cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
-
-	    # comparison
-	    if (dut_output == expected_mav_putvalue):
-		    cocotb.log.info(f'DUT OUTPUT={hex(dut_output)} == EXPECTED={hex(expected_mav_putvalue)} | MATCHED ')
-	    else:
-		    cocotb.log.info(f'opcode={opcode}; func3={func3}; func7={func7}; func7_imm={func7_imm}; func7_2bit={func7_2bit}; func7_1bit={func7_1bit}; func7_fsri_1bit={func7_fsri_1bit}; func7_imm_SHFL={func7_imm_SHFL}; imm_value={imm_value}; imm_value_1={imm_value_1}; fsr_imm_value={fsr_imm_value}; fsr_imm_value={fsr_imm_value}; imm_value={imm_value}; shamt_imm={shamt_imm}; shamt1={shamt1}')
-		    cocotb.log.info(f'DUT OUTPUT={hex(dut_output)} != EXPECTED={hex(expected_mav_putvalue)} | NOT_MATCHED ')
-		    cocotb.log.info('**************************************************')
-	    error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
-	    assert dut_output == expected_mav_putvalue, error_message
-	    cocotb.log.info('--------------------------------------------------')
+        # comparison
+        if (dut_output == expected_mav_putvalue):
+            cocotb.log.info(
+                f'DUT OUTPUT={hex(dut_output)} == EXPECTED={hex(expected_mav_putvalue)} | MATCHED ')
+        else:
+            cocotb.log.info(f'opcode={opcode}; func3={func3}; func7={func7}; func7_imm={func7_imm}; func7_2bit={func7_2bit}; func7_1bit={func7_1bit}; func7_fsri_1bit={func7_fsri_1bit}; func7_imm_SHFL={func7_imm_SHFL}; imm_value={imm_value}; imm_value_1={imm_value_1}; fsr_imm_value={fsr_imm_value}; fsr_imm_value={fsr_imm_value}; imm_value={imm_value}; shamt_imm={shamt_imm}; shamt1={shamt1}')
+            cocotb.log.info(
+                f'DUT OUTPUT={hex(dut_output)} != EXPECTED={hex(expected_mav_putvalue)} | NOT_MATCHED ')
+            cocotb.log.info(
+                '**************************************************')
+        error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
+        assert dut_output == expected_mav_putvalue, error_message
+        cocotb.log.info('--------------------------------------------------')
+        cr += 1
